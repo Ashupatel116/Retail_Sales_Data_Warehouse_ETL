2827a561110e8807899196ffaff3bb35a8a486a0
Ashutosh patel <patelaashu116@gmail.com>
2026-08-18 23:59:33 +0530

    Move database credentials to environment variables

--- src/load.py
+++ src/load.py
@@ -1,3 +1,19 @@
+import mysql.connector
+from config.config import DB_CONFIG
+
+
+def get_connection():
+
+    connection = mysql.connector.connect(
+        host=DB_CONFIG["host"],
+        user=DB_CONFIG["user"],
+        password=DB_CONFIG["password"],
+        database=DB_CONFIG["database"]
+    )
+
+    return connection
+
+
 def load_dim_customer(df, connection):
 
     cursor = connection.cursor()
